1272b8d4b75af43f0ed02a218091c99c08d5620b
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-10-24 15:48:31 +0530

    starting with auto keyword

--- iterator.cpp
+++ iterator.cpp
@@ -13,15 +13,32 @@ void printVec(vector<int> a)
 
 int main() {
 
-    vector<int> v{1, 2, 3, 4, 5, 6};
-    printVec(v);
+    // vector<int> v{1, 2, 3, 4, 5, 6};
+    // printVec(v);
 
-    vector<int> :: iterator it = v.begin();
+    // vector<int> :: iterator it = v.begin();
 
-    cout << (*it) << endl;
-    cout << (*(it+1)) << endl;
+    // cout << (*it) << endl;
+    // cout << (*(it+1)) << endl;
 
-    for (it = v.begin(); it != v.end(); it++) {
-        cout << *it << endl;
+    // for (it = v.begin(); it != v.end(); it++) {
+    //     cout << *it << endl;
+    // }
+
+    vector<pair<int, int>> v_p = {{2, 3}, {3, 4}, {4, 5}};
+    // vector<pair<int, int>> :: iterator it1;
+
+    cout << "first iteration" << endl;
+    for(auto it1 = v_p.begin(); it1 != v_p.end(); it1++) {
+        cout << (*it1).first << " " << (*it1).second << endl;
     }
+
+    cout << "\n";
+
+    cout << "second iteration" << endl;
+    for(auto it1 = v_p.begin(); it1 != v_p.end(); it1++) {
+        cout << it1 -> first << " " << it1 -> second << endl;
+    }
+
+    // (*it).first <=> it -> first
 }
