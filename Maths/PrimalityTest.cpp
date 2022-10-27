4a746b84b1136a3dfa0d3979f7df5dec45585d9d
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-10-27 12:57:35 +0530

    method 1 prime test

---
+++ Maths/PrimalityTest.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// method 1
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    if (n <= 1) {
+        cout << "Provide a valid number" << endl;
+    }
+
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            cout << "Composite \n";
+            return 0;
+        }
+    }
+    cout << "Prime \n";
+    return 0;
+
+    // TC :- O(n) and SC :- O(1)
+}
